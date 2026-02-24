8cd645e1168adbdc874764ed5038ff14bbc49025
Leonardo Riello <leo@Mac.fritz.box>
2026-02-24 20:19:09 +0100

    Fix admin contact email to leo@scola-mia.com across site

--- assets/js/public-api.js
+++ assets/js/public-api.js
@@ -4,7 +4,7 @@ export const CONTACTS = [
   { label: "Instagram", value: "@scolamia", href: "https://www.instagram.com/scolamia/" },
   { label: "Email info", value: "info@scola-mia.com", href: "mailto:info@scola-mia.com" },
   { label: "Proposte articoli", value: "propostearticoli@scola-mia.com", href: "mailto:propostearticoli@scola-mia.com" },
-  { label: "Contatti admin", value: "leo@scolamia.com", href: "mailto:leo@scolamia.com" }
+  { label: "Contatti admin", value: "leo@scola-mia.com", href: "mailto:leo@scola-mia.com" }
 ];
 
 export async function getPublishedArticles() {
